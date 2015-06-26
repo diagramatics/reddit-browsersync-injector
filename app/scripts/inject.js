826b3435f1546f3c1ee682d56a62886ca665e277
Steven Sinatra <ohai@diagramatics.me>
2015-06-27 03:39:09 +1000

    Remove livereload dev capability

--- app/scripts/inject.js
+++ app/scripts/inject.js
@@ -1,32 +1,29 @@
 'use strict';
 
 chrome.runtime.sendMessage({ 'inject': true }, function() {
-	var readyStateCheckInterval = setInterval(function() {
-		if (document.readyState === 'complete') {
-			clearInterval(readyStateCheckInterval);
-			chrome.storage.sync.get({
-				'active': true,
-				'stylePort': 3500,
-				'bsPort': 3501
-			}, function(response) {
-				if (response.active) {
-					var injectedStyle = '<link rel="stylesheet" href="http://localhost:'+ response.stylePort +'/css/style.css" title="applied_subreddit_stylesheet" type="text/css">';
+	chrome.storage.sync.get({
+		'active': true,
+		'stylePort': 3500,
+		'bsPort': 3501
+	}, function(response) {
+		if (response.active) {
+			var injectedStyle = '<link rel="stylesheet" href="//localhost:'+ response.stylePort +'/css/style.css" title="applied_subreddit_stylesheet" type="text/css">';
 
-					var body = document.querySelector('body');
-					body.insertAdjacentHTML('beforeend', '<script async src="//localhost:'+ response.bsPort +'/browser-sync/browser-sync-client.js"><\/script>');
+			var body = document.querySelector('body');
+			var script = document.createElement('script');
+			script.src = '//localhost:'+ response.bsPort +'/browser-sync/browser-sync-client.js';
+			document.head.appendChild(script);
 
-					// Find the custom subreddit styling
-					var style = document.querySelector('link[title="applied_subreddit_stylesheet"]');
-					if (style !== null) {
-						style.insertAdjacentHTML('afterend', injectedStyle);
-						style.remove();
-					}
-					else {
-						var head = document.querySelector('head');
-						head.insertAdjacentHTML('beforeend', injectedStyle);
-					}
-				}
-			});
+			// Find the custom subreddit styling
+			var style = document.querySelector('link[title="applied_subreddit_stylesheet"]');
+			if (style !== null) {
+				style.insertAdjacentHTML('afterend', injectedStyle);
+				style.remove();
+			}
+			else {
+				var head = document.querySelector('head');
+				head.insertAdjacentHTML('beforeend', injectedStyle);
+			}
 		}
-	}, 10);
+	});
 });
